35ba5f455a431b9d606e5bd65423fc3c232481d5
Israel Lucena <israelclucena@gmail.com>
2025-01-27 21:47:58 +0000

    Seção 6: Working with Dynamic Content & Adding Templating Engines

--- course-project/app.js
+++ course-project/app.js
@@ -1,10 +1,12 @@
 const path = require("path");
 
+const express = require("express");
 const bodyParser = require("body-parser");
 
-const express = require("express");
-
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -16,7 +18,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
